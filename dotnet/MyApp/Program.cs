9285ac6d265ccc3a3c6057f1d702b13dac20fca0
adriana-diaz <122817007+adriana-diaz@users.noreply.github.com>
2023-09-06 13:27:29 -0600

    dotnet app

---
+++ dotnet/MyApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre = "World";
+            if (args.Length > 0){
+                nombre = args[0];
+            }
+
+            Console.WriteLine(String.Format("Hola :) {0}, bienvenido!", nombre));
+        }
+    }
+}
